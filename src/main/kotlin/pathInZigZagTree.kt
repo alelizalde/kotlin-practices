828fc793b61ca1c31b024d935a8605e61fc081bb
Al <popcornbyte@gmail.com>
2019-06-30 22:22:26 -0700

    Optimizing code

--- src/main/kotlin/pathInZigZagTree.kt
+++ src/main/kotlin/pathInZigZagTree.kt
@@ -1,9 +1,13 @@
 
 import kotlin.math.*
 
+
+
 fun main() {
     //println(pathInZigZagTree(14))
-    println(pathInZigZagTreeBinaryLevel(14))
+    //println(pathInZigZagTreeBinaryLevel(26))
+    //println(pathInZigZagTree4(14))
+    println(pathInZigZagTree5(14))
 }
 
 fun pathInZigZagTreeBinaryLevel(label: Int): List<Int> {
@@ -13,8 +17,9 @@ fun pathInZigZagTreeBinaryLevel(label: Int): List<Int> {
         ret.add(0, cur)
         if (cur == 1) break
         cur /= 2
-        var h = Integer.highestOneBit(cur)
-        cur = cur xor h - 1
+        var height = Integer.highestOneBit(cur)
+        println("$cur, ${height-1}")
+        cur = cur xor height - 1
     }
     return ret
 }
@@ -73,4 +78,22 @@ fun pathInZigZagTree4(label: Int): List<Int> {
         if (i > 0 && (i + res.size) % 2 == 0)
             res[i] = res[i] xor (1 shl i) - 1
     return res
+}
+
+fun pathInZigZagTree5(label: Int): List<Int> {
+    var label = label
+    val result = mutableListOf<Int>()
+    result.add(0, label) //taking bottom up approach, so the label itself must be included
+    var height = log2(label.toDouble()).toInt() //height of label from root
+
+    while (height != 0) {
+        val left = 2.00.pow(height - 1).toInt() // rank of left limit of the previous level
+        val right = 2.00.pow(height).toInt() - 1// rank of right limit of the previous level
+        label = left + (right - label / 2)// The previous level is sorted in different order than current one.
+        //So the new label is adjusted accordingly
+        result.add(0, label)  //newly found label is appened to result
+        height--
+    }
+
+    return result
 }
